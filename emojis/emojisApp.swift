a4ac51f5e5e95ebea345e016c8deb474fa45fd96
Wildan <wildanalifrio@gmail.com>
2024-01-21 10:11:00 +0700

    Initial Commit

---
+++ emojis/emojisApp.swift
@@ -0,0 +1,17 @@
+//
+//  emojisApp.swift
+//  emojis
+//
+//  Created by Wildan on 21/01/24.
+//
+
+import SwiftUI
+
+@main
+struct emojisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
